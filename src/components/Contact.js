65df8bdcc65b10ff427df79ebd6c123f283ddcce
chloe-tt <61197607+chloe-tt@users.noreply.github.com>
2020-03-26 16:40:09 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,22 @@
-import React from 'react';
-import './Contact.css'
-import { checkPropTypes } from 'prop-types';
+import React from "react";
+import "./Contact.css";
 
-
-function Contact(prop) {
+function Contact(props) {
   return (
-
-<div className="Contact">
-    <div className="avatar"> 
-    <img src={props.avatar} alt={props.name} />
-       </div>
-    <div className="container">
-        <div className="name">
-            <h2>{props.name}</h2>
-        </div>
+    <div className="Contact">
+      <img
+        src={props.avatar}
+        alt={props.name}
+      />
+      <div>
+        <p className="name">{props.name}</p>
         <div className="status">
-            <div className={props.status? "status-online" : "status-offline"}></div>
-            <div className="status-text">
-                <p>{props.online}</p>
-            </div>
+          <div className{props.status ? "status-online" : "status-offline"}/>
+          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
         </div>
+      </div>
     </div>
-</div>
-
   );
 }
 
 export default Contact;
-
