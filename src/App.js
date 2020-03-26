ef432908309da21476dbb03ef16730d36d920127
chloe-tt <61197607+chloe-tt@users.noreply.github.com>
2020-03-26 16:42:42 +0100

    dsqsq

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="App">
       <Contact 
+      name = "David Lolilol"
+      avatar ="https://randomuser.me/api/portraits/men/34.jpg"
+      online
+      status
+      />
+    <div className="App">
+      <Contact 
+      name = "Camile Mdr"
+      avatar ="https://randomuser.me/api/portraits/women/34.jpg"
+      />
+     <Contact 
+      name = "Marine Pozo"
+      avatar ="https://randomuser.me/api/portraits/women/4.jpg"
       />
     </div>
   );
